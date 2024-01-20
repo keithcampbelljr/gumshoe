63ff8a0bd8b2bd861c2bc10e4058232a68ce6516
keithcampbelljr <keithcampbelljr@icloud.com>
2024-01-19 21:42:45 -0500

    refactor, cleaned up and added typing

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from utils import perform_port_scan, sanitize_scan_results, check_production_endpoints
+import logging
+from utils import perform_nmap_scan, sanitize_scan_results, check_production_endpoints
+from typing import Dict
 
-target_ip = "192.168.1.1/32"
-scan_output = perform_port_scan(target_ip)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-sanitized_results = sanitize_scan_results(scan_output)
+def main() -> None:
+    target_ip: str = "192.168.1.0/24"
+    
+    try:
+        scan_output: Dict[str, Dict] = perform_nmap_scan(target_ip)
+        sanitized_results: Dict[str, Dict] = sanitize_scan_results(scan_output)
+        check_production_endpoints(sanitized_results)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
 
-check_production_endpoints(sanitized_results)
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,18 +1,46 @@
+import logging
 import nmap3
+from typing import Dict
 
-def perform_port_scan(target):
+logger = logging.getLogger(__name__)
+
+def perform_nmap_scan(target: str) -> Dict[str, Dict]:
+    """
+    Perform an Nmap scan on the specified target.
+
+    Args:
+        target (str): The target IP address or range.
+
+    Returns:
+        Dict[str, Dict]: Nmap scan results.
+    """
     nmap_discovery = nmap3.NmapHostDiscovery()
-    scan_output = nmap_discovery.nmap_portscan_only(target)
+    scan_output: Dict[str, Dict] = nmap_discovery.nmap_no_portscan(target)
     return scan_output
 
-def sanitize_scan_results(scan_results):
+def sanitize_scan_results(scan_results: Dict[str, Dict]) -> Dict[str, Dict]:
+    """
+    Remove unnecessary information from Nmap scan results.
+
+    Args:
+        scan_results (Dict[str, Dict]): Nmap scan results.
+
+    Returns:
+        Dict[str, Dict]: Sanitized scan results.
+    """
     keys_to_sanitize = ["runtime", "stats", "task_results"]
-    sanitized_results = scan_results.copy()
+    sanitized_results: Dict[str, Dict] = scan_results.copy()
     for key_to_remove in keys_to_sanitize:
         sanitized_results.pop(key_to_remove, None)
     return sanitized_results
 
-def check_production_endpoints(scan_results):
+def check_production_endpoints(scan_results: Dict[str, Dict]) -> None:
+    """
+    Check the status of production endpoints based on Nmap scan results.
+
+    Args:
+        scan_results (Dict[str, Dict]): Nmap scan results.
+    """
     for host, scan_result in scan_results.items():
         if scan_result['state']['state'] == 'up':
-            print(f"The production endpoint {host} is reachable.")
+            logger.info(f"The production endpoint {host} is reachable.")
